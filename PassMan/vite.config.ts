7600826c0997472f3946f59dabee43a8d7562c26
Artur <artur.cha@outlook.com>
2025-11-20 11:12:32 +0100

    Completed config

---
+++ PassMan/vite.config.ts
@@ -0,0 +1,29 @@
+import { defineConfig } from "vite";
+import { resolve } from "path";
+import { viteStaticCopy } from "vite-plugin-static-copy";
+
+export default defineConfig({
+ plugins: [
+    (viteStaticCopy({
+      targets: [
+        { src: "public/manifest.json", dest: "." }
+      ]
+    }) as unknown) as any
+  ],
+  build: {
+    outDir: "dist",
+    sourcemap: false,
+    rollupOptions: {
+      input: {
+        popup: resolve(__dirname, "src/popup.html"),
+        background: resolve(__dirname, "src/background.ts"),
+        contentScript: resolve(__dirname, "src/contentScript.ts"),
+      },
+      output: {
+        entryFileNames: `[name].js`,
+        chunkFileNames: `[name].js`,
+        assetFileNames: `[name].[ext]`
+      }
+    }
+  }
+});
